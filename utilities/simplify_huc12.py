c38d844188ee67e45902c2ce2a4a5066a5f34a02
kyleredilla <kmredilla@alaska.edu>
2022-03-07 12:05:36 -0900

    drop unused columns and rows from huc12 and rename

--- utilities/simplify_huc12.py
+++ utilities/simplify_huc12.py
@@ -6,8 +6,14 @@ Assumes original WGS84 HUC-12 shapefile is present in working directory.
 import geopandas as gpd
 
 
-huc12_gdf = gpd.read_file("hydrologic_units_wbdhu12_a_ak.shp").to_crs("EPSG:3338")
+# default name of file
+huc12_gdf = gpd.read_file("wbdhu12_a_ak.shp").to_crs(3338)
 new_huc_geoms = huc12_gdf["geometry"].simplify(100, preserve_topology=True)
 new_gdf = huc12_gdf.copy()
 new_gdf["geometry"] = new_huc_geoms
-new_gdf.to_file("hydrologic_units_wbdhu12_a_ak_simplified.shp")
+new_gdf = new_gdf.rename(columns={"huc12": "id"})
+new_gdf = new_gdf[["id", "name", "states", "geometry"]]
+# drop cook inlet, kotzebue sound, Aleutians HUC, St Lawrence Island
+drop_hucs = ["190208000003", "190505000000", "190301030000", "190501010000"]
+new_gdf = new_gdf[~new_gdf["id"].isin(drop_hucs)]
+new_gdf.to_file("ak_huc12s.shp")
